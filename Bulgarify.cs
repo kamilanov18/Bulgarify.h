2db38f3a6ded6cbecefd758fdc4e7e0363a6eb1a
SNKolev18 <SNKolev18@codingburgas.bg>
2021-09-28 13:16:57 +0300

    Start C# implementation of Bulgarify.h
    Co-Authored-By: Emil Ribarev <idiliev18@codingburgas.bg>

---
+++ Bulgarify.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Система = System;
+using Конзола = System.Console;
+using Целочислен_тип = System.Int32;
+using Двоен_реален_тип = System.Double;
+using Низ = System.String;
+using Летящ_реален_тип = System.Float;
+
+
+void Напиши<T>(T args) 
+{
+	Console.Write(args);
+}
+
+void НапишиРед<T>(T args)
+{
+	Console.Write(args);
+}
+
