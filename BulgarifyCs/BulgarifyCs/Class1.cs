1152820d31bdf34db6053776c035612e6754e8b7
idiliev18 <idiliev18@codingburgas.bg>
2021-10-31 13:37:19 +0200

    Add String.IndexOf method with 10 overloads

--- BulgarifyCs/BulgarifyCs/Class1.cs
+++ BulgarifyCs/BulgarifyCs/Class1.cs
@@ -21,5 +21,45 @@ namespace BulgarifyCs
         {
             return str.Contains(value);
         }
+        public static int ПозицияНа(this string str, String value, StringComparison comparisonType)
+        {
+            return str.IndexOf(value, comparisonType);
+        }
+        public static int ПозицияНа(this string str, String value, int startIndex, StringComparison comparisonType)
+        {
+            return str.IndexOf(value, startIndex, comparisonType);
+        }
+        public static int ПозицияНа(this string str, String value, int startIndex, int count, StringComparison comparisonType)
+        {
+            return str.IndexOf(value, startIndex, count, comparisonType);
+        }
+        public static int ПозицияНа(this string str, String value, int startIndex, int count)
+        {
+            return str.IndexOf(value, startIndex, count);
+        }
+        public static int ПозицияНа(this string str, String value, int startIndex)
+        {
+            return str.IndexOf(value, startIndex);
+        }
+        public static int ПозицияНа(this string str, String value)
+        {
+            return str.IndexOf(value);
+        }
+        public static int ПозицияНа(this string str, char value, StringComparison comparisonType)
+        {
+            return str.IndexOf(value, comparisonType);
+        }
+        public static int ПозицияНа(this string str, char value, int startIndex, int count)
+        {
+            return str.IndexOf(value, startIndex, count);
+        }
+        public static int ПозицияНа(this string str, char value, int startIndex)
+        {
+            return str.IndexOf(value, startIndex);
+        }
+        public static int ПозицияНа(this string str, char value)
+        {
+            return str.IndexOf(value);
+        }
     }
 }
--- BulgarifyCs/BulgarifyCsTest/Program.cs
+++ BulgarifyCs/BulgarifyCsTest/Program.cs
@@ -9,7 +9,9 @@ namespace BulgarifyCsTest
     {
         static void Main(string[] args)
         {
-            
+            var низ = "Доко, Доко, Доко, Кайба шеску калумач";
+            низ.ПозицияНа("Доко");
+            низ.Съдържа("Доко");
         }
     }
 }
