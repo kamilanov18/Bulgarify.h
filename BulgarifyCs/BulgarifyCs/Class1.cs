dec48d3c1b5f72d62a5c60a27eb852f1fb31a441
idiliev18 <idiliev18@codingburgas.bg>
2021-10-31 13:27:28 +0200

    Add String.Contains method

---
+++ BulgarifyCs/BulgarifyCs/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using низ = global::System.String;
+
+namespace BulgarifyCs
+{
+    public static class BulgarifyString
+    {
+        public static bool Съдържа(this string str, String value, StringComparison comparisonType)
+        {
+            return str.Contains(value, comparisonType);
+        }
+        public static bool Съдържа(this string str, char value, StringComparison comparisonType)
+        {
+            return str.Contains(value, comparisonType);
+        }
+        public static bool Съдържа(this string str, char value)
+        {
+            return str.Contains(value);
+        }
+        public static bool Съдържа(this string str, String value)
+        {
+            return str.Contains(value);
+        }
+    }
+}
---
+++ BulgarifyCs/BulgarifyCsTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BulgarifyCs;
+
+
+
+namespace BulgarifyCsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
